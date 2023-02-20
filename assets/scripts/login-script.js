55ff4c9b4498d7bae6e53b534b4c26f394b35bfe
Vinsensius Adrian Wijaya <95972062+XavierMexious@users.noreply.github.com>
2023-02-20 22:52:47 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -9,17 +9,28 @@ const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// Membuat variabel untuk menyimpan informasi email dan password.
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/*Comment : Menambahkan aksi klik pada button*/
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
+    /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan */
+    if (email ==expectedEmail && password == expectedPassword) {
+      goToHome();
+    } else {
+      // Namun jika tidak sesuai maka menampilkan informasi input salah
+      showPopUp();
+    }
 });
